caea176c3dc6b9aee172fdd05d0ac1af8348b90a
bearddan2000 <bearddan2000@yahoo.com>
2022-03-26 14:10:31 -0400

    initial commit

---
+++ java-cli-gradle-spring-cucumber-mockito-testng-car-data/bin/src/test/java/RunCucumberTest.java
@@ -0,0 +1,14 @@
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+@Test
+public class RunCucumberTest extends AbstractTestNGCucumberTests {
+
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+}
